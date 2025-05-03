3088cc49647d87bd58061113cf189d5b9e07feac
jaykharpuse <ap2730515@gmail.com>
2025-05-03 19:44:52 +0530

    fix: set correct CORS origin for Vercel

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,7 +4,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://code-reviewer-ruby.vercel.app',
+    credentials: true
+  }));
 
 
 app.use(express.json())
